f17a04a830af3e069b0007c976078b35695d50b6
CharlesEtienneTremblay <cetremblay2011@gmail.com>
2025-02-25 08:41:13 -0500

    Ajout de l'header

--- trpr01/src/App.vue
+++ trpr01/src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import Header from "./components/Header.vue"
+import ProductManager from "./components/ProductManager.vue"
 </script>
 
-<template></template>
+<template>
+  <Header></Header>
+  <ProductManager></ProductManager>
+</template>
 
 <style scoped></style>
---
+++ trpr01/src/components/Header.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <header class="text-light text-center">
+    <h1>Gestionnaire de produits</h1>
+  </header>
+</template>
+<style scoped></style>
